2a068ba40e5588ea7817dc99905492a8b7a546bd
MalcolmScruggs <mgscruggs@gmail.com>
2017-05-05 03:18:54 -0400

    S07

--- S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -15,19 +15,69 @@
  */
 package com.example.android.sunshine.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.sunshine.data.WeatherContract.WeatherEntry;
+
 /**
  * Manages a local database for weather data.
  */
-// TODO (11) Extend SQLiteOpenHelper from WeatherDbHelper
-public class WeatherDbHelper {
 
-//  TODO (12) Create a public static final String called DATABASE_NAME with value "weather.db"
+// COMPLETED (11) Extend SQLiteOpenHelper from WeatherDbHelper
+public class WeatherDbHelper extends SQLiteOpenHelper {
 
-//  TODO (13) Create a private static final int called DATABASE_VERSION and set it to 1
+    //  COMPLETED (12) Create a public static final String called DATABASE_NAME with value "weather.db"
+    /*
+     * This is the name of our database. Database names should be descriptive and end with the
+     * .db extension.
+     */
+    public static final String DATABASE_NAME = "weather.db";
 
-//  TODO (14) Create a constructor that accepts a context and call through to the superclass constructor
+    //  COMPLETED (13) Create a private static final int called DATABASE_VERSION and set it to 1
+    /*
+     * If you change the database schema, you must increment the database version or the onUpgrade
+     * method will not be called.
+     *
+     * The reason DATABASE_VERSION starts at 3 is because Sunshine has been used in conjunction
+     * with the Android course for a while now. Believe it or not, older versions of Sunshine
+     * still exist out in the wild. If we started this DATABASE_VERSION off at 1, upgrading older
+     * versions of Sunshine could cause everything to break. Although that is certainly a rare
+     * use-case, we wanted to watch out for it and warn you what could happen if you mistakenly
+     * version your databases.
+     */
+    private static final int DATABASE_VERSION = 1;
 
-//  TODO (15) Override onCreate and create the weather table from within it
+    //  COMPLETED (14) Create a constructor that accepts a context and call through to the superclass constructor
+    public WeatherDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_ENTRIES =
+                "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
+                        WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        WeatherEntry.COLUMN_DATE + " INTEGER, " +
+                        WeatherEntry.COLUMN_WEATHER_ID + " INTEGER, " +
+                        WeatherEntry.COLUMN_MIN_TEMP + " REAL, " +
+                        WeatherEntry.COLUMN_MAX_TEMP + " REAL, " +
+                        WeatherEntry.COLUMN_HUMIDITY + " REAL, " +
+                        WeatherEntry.COLUMN_PRESSURE + " REAL, " +
+                        WeatherEntry.COLUMN_WIND_SPEED + " REAL, " +
+                        WeatherEntry.COLUMN_DEGREES + " REAL" + ");";
+
+
+
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+
+    //  TODO (15) Override onCreate and create the weather table from within it
 
 //  TODO (16) Override onUpgrade, but don't do anything within it yet
 }
